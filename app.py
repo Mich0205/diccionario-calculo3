72eac3843acfd31eab184f37b893f90334eb518a
Mich0205 <322315338@pcpuma.acatlan.unam.mx>
2025-11-27 15:24:26 -0600

    Update title in app.py for clarity

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Diccionario Cálculo 3 Completo", layout="wide")
 
-st.title("📘 Diccionario Completo de Cálculo Multivariable")
+st.title("📘 Diccionario Completo de Cálculo")
 st.write("Selecciona un término en la barra lateral (sidebar) para ver su concepto.")
 
 # ==================================================
